a1dbde2505e1c87246dbef036e1eeaf082353097
saurav dahal <d.saurab1@gmail.com>
2024-02-03 21:50:02 -0500

    feat: Added birthplace controller and view

---
+++ Website API/Controllers/BirthplaceController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website_API.Controllers
+{
+    public class BirthplaceController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
